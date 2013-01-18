62f75957c99e25877539dc661012c2ff0495a2df
Michael Whittaker <post@michael-whittaker.de>
2013-01-18 14:39:21 +0100

    adds rails 4 (4.0.0.beta) project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Provo::Application.config.secret_key_base = '8e53ace66eeea6fabee0c6895a55cd69125fdbd3ee5746fe264c7e4eb4e9a6e185120a59424f7b2070882de227a8a14a8b70e067d1cf24e0d69fc670cfb9e23a'
