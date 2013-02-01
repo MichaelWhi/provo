7bed1e85c47a4fa7d1af375d4afccde9a8c8a8f5
Michael Whittaker <post@michael-whittaker.de>
2013-02-01 23:17:57 +0100

    tries to enable font awesome at heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Provo::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
