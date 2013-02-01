8d1c5162faa5ca6bca0b16b754e56596f65377a4
Michael Whittaker <post@michael-whittaker.de>
2013-02-01 23:50:39 +0100

    changes production server to unicorn

---
+++ lib/unicorn/config.rb
@@ -0,0 +1,8 @@
+# What the timeout for killing busy workers is, in seconds
+timeout 60
+ 
+# Whether the app should be pre-loaded
+preload_app true
+ 
+# How many worker processes
+worker_processes 2
