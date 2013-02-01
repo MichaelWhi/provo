d28291f4777257ab83f74c208726de2233a7d2f8
Michael Whittaker <post@michael-whittaker.de>
2013-02-01 23:59:38 +0100

    Revert "changes log level to debug for prod env"
    This reverts commit 1040aae7cc70b299a462429e458be2c137cd3296.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Provo::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug # #DEBUG
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
