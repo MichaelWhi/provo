52592dca30817faeb70d2aca60735078a9324347
Michael Whittaker <post@michael-whittaker.de>
2013-03-01 15:53:16 +0100

    adds save to pdf link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def save_as_pdf_link(text = "Save as PDF")
+    link_to text, "http://pdfcrowd.com/url_to_pdf/?use_print_media=1&width=210mm&height=297mm&footer_text=%u,%20page%20%p"
+  end
 end
